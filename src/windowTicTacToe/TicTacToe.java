b9c5ba5189b91698464a9ee84b4c3853df5f0e2f
Dmitri <rauckees0811@gmail.com>
2023-12-30 22:20:57 +0300

    add methods draw

--- src/windowTicTacToe/TicTacToe.java
+++ src/windowTicTacToe/TicTacToe.java
@@ -9,12 +9,50 @@ public class TicTacToe extends JComponent {
     public static final int FIELD_X = 1;
     public static final int FIELD_0 = 2;
     int[][] field;
+    boolean key = true;
     public TicTacToe(){
         enableEvents(AWTEvent.MOUSE_EVENT_MASK);
     }
     @Override
     protected void processMouseEvent(MouseEvent mouseEvent){
         super.processMouseEvent(mouseEvent);
+        if(mouseEvent.getButton() == MouseEvent.BUTTON1){
+            int x = mouseEvent.getX();
+            int y = mouseEvent.getY();
+            int i = (int)((float) x / getWidth() * 3);
+            int j = (int)((float) y / getHeight() * 3);
+            if(field[i][j] == FIELD_EMPTY){
+                if(key){
+                }
+                else if(!key){
+
+                }
+            }
+        }
+    }
+    void drawX0(Graphics graphics){
+        for(int i = 0; i < 3; i ++){
+            for(int j = 0; j < 3; j ++){
+
+            }
+        }
+    }
+    void drawX(int i, int j, Graphics graphics){
+        graphics.setColor(Color.BLUE);
+        int dw = getWidth() / 3;
+        int dh = getHeight() / 3;
+        int x = i * dw;
+        int y = j * dh;
+        graphics.drawLine(x, y, x + dw , y + dh);
+        graphics.drawLine(x, y + dh, x + dw , y);
+    }
+    void draw0(int i, int j, Graphics graphics){
+        graphics.setColor(Color.RED);
+        int dw = getWidth() / 3;
+        int dh = getHeight() / 3;
+        int x = i * dw;
+        int y = j * dh;
+        graphics.drawOval(x + 5 * dw/100, y, dw * 9 / 10, dh);
     }
     @Override
     protected void paintComponent(Graphics graphics){
