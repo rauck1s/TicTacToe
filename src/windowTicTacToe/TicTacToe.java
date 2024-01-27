cec921fffbb80fa4aadd1f46cfed06a2b415ca77
Dmitri <rauckees0811@gmail.com>
2024-01-27 09:40:33 +0300

    add move methods(first and second)

--- src/windowTicTacToe/TicTacToe.java
+++ src/windowTicTacToe/TicTacToe.java
@@ -9,10 +9,21 @@ public class TicTacToe extends JComponent {
     public static final int FIELD_X = 1;
     public static final int FIELD_0 = 2;
     int[][] field;
-    boolean key = true;
+    int key = 0;
     public TicTacToe(){
         enableEvents(AWTEvent.MOUSE_EVENT_MASK);
+        field = new int[3][3];
+        initGame();
     }
+
+    public void initGame() {
+        for(int i =0; i < 3; i ++){
+            for(int j = 0; j < 3; j ++){
+                field[i][j] = FIELD_EMPTY;
+            }
+        }
+    }
+
     /*клики мышки*/
     @Override
     protected void processMouseEvent(MouseEvent mouseEvent){
@@ -23,23 +34,53 @@ public class TicTacToe extends JComponent {
             int i = (int)((float) x / getWidth() * 3);
             int j = (int)((float) y / getHeight() * 3);
             if(field[i][j] == FIELD_EMPTY){
-                if(key){
-                }
-                else if(!key){
-
-                }
+                field[i][j] = FIELD_X;
             }
         }
     }
+
     /*отвечает за рисовку ноликов и крестиков, но подумать надо про алгоритм с раставлением ноликов(как передать ячейку,
     * как определять где крестики и как лучше ставить нолики)*/
     void drawX0(Graphics graphics){
         for(int i = 0; i < 3; i ++){
             for(int j = 0; j < 3; j ++){
-
+                if(field[i][j] == FIELD_X){
+                    drawX(i, j, graphics);
+                    search_0(i, j, graphics);
+                    key++;
+                }
             }
         }
     }
+
+    public void search_0(int i, int j, Graphics graphics) {
+        switch (key){
+            case 1 ->{
+                moveFirst(i,j,graphics);
+            }
+            case 2 ->{
+                moveSecond(i,j,graphics);
+            }
+            case 3->{
+
+            }
+
+        }
+    }
+
+    public void moveSecond(int i, int j, Graphics graphics) {
+
+    }
+
+    public void moveFirst(int i, int j, Graphics graphics){
+        if(field[1][1] != FIELD_X){
+            draw0(1,1,graphics);
+        }
+        else {
+            draw0(0,2,graphics);
+        }
+    }
+
     /*ставит крестики*/
     void drawX(int i, int j, Graphics graphics){
         graphics.setColor(Color.BLUE);
